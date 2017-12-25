16832f07630d97164663d2002d37afaa78c6e886
wengy233 <1197661594@qq.com>
2017-12-25 16:41:38 +0800

    框架

---
+++ tp.h
@@ -0,0 +1,21 @@
+#ifndef tp_h
+#define tp_h
+const int maxVertices 10;
+const int maxWeight=INT_MAX;
+using namespace std;
+struct Router{
+    int number;    //路由表编号
+    string address;   //路由表地址
+};
+class Graphmtx{
+protected:
+    int numVertices;    //当前顶点数
+    int numEdges;      //当前边数
+    string *VerticesList;    //对应address
+    int *p;          //对应number
+    int **Edge;      //权值
+public:
+    Graphmtx();   //初始化
+    ～Graphmtx();
+};
+#endif /* tp_h */

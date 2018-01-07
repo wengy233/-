47cecbed75d1ce3d934cdd6c47770d4af99d4248
wengy233 <34634836+wengy233@users.noreply.github.com>
2018-01-08 00:26:31 +0800

    Update tp.h

--- tp.h
+++ tp.h
@@ -23,8 +23,6 @@ public:
     bool insertEdge(int v1,int v2,int cost);
     bool removeVertex(int v);//删除顶点 
     bool removeEdge(int v1,int v2);//删除边 
-    void remove(int v);//删除某路由器 
-    void remove2(int v1,int v2);//删除两路由器间的链路 
     void Shortest(int n);
     void printPath(int v,int dist[],int path[]);
     void shuchu();
